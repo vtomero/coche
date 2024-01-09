347701dbd43cc652b1020cf017eeff7c4c98b4d4
vtomero <75685527+vtomero@users.noreply.github.com>
2024-01-09 18:41:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,39 @@
 from machine import Pin,UART, PWM
 from time import sleep
 from coche import Coche
-uart = UART(0, baudrate=9600, tx=Pin(12), rx=Pin(13))
+uart = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
 
 led = Pin(25, Pin.OUT) 
 coche = Coche(1,2,27,26,0,28)
 
 while True:
     
-    if uart.any(): #Devulve cero si no hay nada que transmitir
-        data = uart.read() #devuelve el objeto leido
-        if data== b'F\r\n':                      
+    if uart.any(): #Devuelve 0 sino hay datos que mandar
+        data = uart.read() #devuleve el objeto con la lectura de los datos        
+        if data== b'f\r\n' or data ==b'f':                      
             led.high() 
             print("Coche Adelante")
             uart.write("LED encendido")
-            for n in range(3,10):
-                coche.setvelocidad(n*10000)           
-                coche.irapor('ATRAS',2)               
-                
-           
-        elif data==b'b\r\n':
+            coche.adelante()
+        elif data== b'a\r\n' or data ==b'a':
+            led.high()
+            print("Coche Atrás")
+            uart.write("LED encendido")
+            coche.atras()              
+        elif data== b'i\r\n' or data ==b'i':
+            led.high()
+            print("Coche a la Izquierda")
+            uart.write("LED encendido")
+            coche.izquierda()
+        elif data== b'd\r\n' or data ==b'd':
+            led.high()
+            print("Coche a la Derecha")
+            uart.write("LED encendido")
+            coche.derecha() 
+        elif data== b's\r\n' or data ==b's':
             led.low()
             print("Coche Parado")
             uart.write("LED apagado")
             coche.stop()
           
+
