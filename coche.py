878f7757adff1a2490716f58a9e68fcb30989f00
vtomero <75685527+vtomero@users.noreply.github.com>
2024-02-01 22:05:30 +0100

    Adicción de sonido

--- coche.py
+++ coche.py
@@ -127,5 +127,136 @@ class Coche:
             self.iratraspor(tiempo)
         else:
             self.stop()
+
+
+'''Clase sonido para el coche mediante un buzzer pasivo.
+duty_u16 para el Volumen, y la frequencia para el tono'''
+
+class Sonido:
+    tiempo = 0.3
+    def __init__(self, buz: int or PWM, duty = 0, freq = 500):
+        if isinstance(buz, int):
+            self.buz = PWM(Pin(buz))
+        if isinstance(buz, PWM):
+            self.buz = buz
+        self.buz.freq(freq)
+        self.buz.duty_u16(duty)
         
+    
+    def tocar(self, freq):
+        self.buz.duty_u16(2000)
+        self.buz.freq(freq)
+    
+    tones = {
+        "P": "P",
+        "B0": 31,
+        "C1": 33,
+        "CS1": 35,
+        "D1": 37,
+        "DS1": 39,
+        "E1": 41,
+        "F1": 44,
+        "FS1": 46,
+        "G1": 49,
+        "GS1": 52,
+        "A1": 55,
+        "AS1": 58,
+        "B1": 62,
+        "C2": 65,
+        "CS2": 69,
+        "D2": 73,
+        "DS2": 78,
+        "E2": 82,
+        "F2": 87,
+        "FS2": 93,
+        "G2": 98,
+        "GS2": 104,
+        "A2": 110,
+        "AS2": 117,
+        "B2": 123,
+        "C3": 131,
+        "CS3": 139,
+        "D3": 147,
+        "DS3": 156,
+        "E3": 165,
+        "F3": 175,
+        "FS3": 185,
+        "G3": 196,
+        "GS3": 208,
+        "A3": 220,
+        "AS3": 233,
+        "B3": 247,
+        "C4": 262,
+        "CS4": 277,
+        "D4": 294,
+        "DS4": 311,
+        "E4": 330,
+        "F4": 349,
+        "FS4": 370,
+        "G4": 392,
+        "GS4": 415,
+        "A4": 440,
+        "AS4": 466,
+        "B4": 494,
+        "C5": 523,
+        "CS5": 554,
+        "D5": 587,
+        "DS5": 622,
+        "E5": 659,
+        "F5": 698,
+        "FS5": 740,
+        "G5": 784,
+        "GS5": 831,
+        "A5": 880,
+        "AS5": 932,
+        "B5": 988,
+        "C6": 1047,
+        "CS6": 1109,
+        "D6": 1175,
+        "DS6": 1245,
+        "E6": 1319,
+        "F6": 1397,
+        "FS6": 1480,
+        "G6": 1568,
+        "GS6": 1661,
+        "A6": 1760,
+        "AS6": 1865,
+        "B6": 1976,
+        "C7": 2093,
+        "CS7": 2217,
+        "D7": 2349,
+        "DS7": 2489,
+        "E7": 2637,
+        "F7": 2794,
+        "FS7": 2960,
+        "G7": 3136,
+        "GS7": 3322,
+        "A7": 3520,
+        "AS7": 3729,
+        "B7": 3951,
+        "C8": 4186,
+        "CS8": 4435,
+        "D8": 4699,
+        "DS8": 4978,
+    }
+    
+    cancion = ["E5","G5","A5","P","E5","G5","B5","A5","P","E5","G5","A5","P","G5","E5"]
+    
+    def silencio(self):
+         self.buz.duty_u16(0)
+    
+    def tocar_cancion(self, sonidos, tiempo):
+        for nota in range(len(sonidos)):
+            if self.tones[sonidos[nota]] == "P":
+                self.silencio()
+            else: self.tocar(self.tones[sonidos[nota]])
+            sleep(tiempo)
+        self.silencio()
+    
+    def iniciar(self, cancion):
+        self.tocar_cancion(cancion)
+    
+    def tocar_la_cancion(self,cancion=cancion, tiempo = tiempo):
+        self.tocar_cancion(cancion, tiempo)
+    
     
